3079a49712c743f6591713c52ded1f31bfddb58f
ajsandovalt <antonio.sandoval@posteo.net>
2026-01-06 21:07:41 -0600

    Changes to themes and other stuff

--- init.lua
+++ init.lua
@@ -911,12 +911,19 @@ require('lazy').setup({
   },
 
   {
-    'miikanissi/modus-themes.nvim',
-    priority = 1000,
+    'zenbones-theme/zenbones.nvim',
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = 'rktjmp/lush.nvim',
     lazy = false,
+    priority = 1000,
     config = function()
-      require('modus-themes').setup { transparent = true }
-      vim.cmd.colorscheme 'modus_operandi'
+      vim.g.zenburned_darken_comments = 35
+      vim.g.zenburned_transparent_background = true
+      vim.cmd.colorscheme 'zenburned'
+      vim.cmd 'set background=dark'
+      vim.cmd 'set termguicolors'
     end,
   },
 
